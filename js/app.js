b10d62bc02e58b98d3d70e56e8ce94f06b560a3d
“markantonio99” <markomakkia@gmail.com>
2023-02-22 00:31:33 +0100

    salv

--- js/app.js
+++ js/app.js
@@ -1,12 +1,37 @@
+const { createApp } = Vue;
 
- 
-  const { createApp } = Vue
-
-  createApp({
-    data() {
-      return {
-        message: 'Hello Vue!'
+createApp({
+  data() {
+    return {
+      message: "Hello Vue!",
+      nuovoImpegno: { text: "", done: false },
+      impegni: [
+        { text: "Comprare il latte", done: false },
+        { text: "Prendere ingredienti per la lasagna", done: true },
+      ],
+      impegniSvolti: []
+    };
+  },
+  methods: {
+    aggiungiImpegno() {
+      this.impegni.push({ text: this.nuovoImpegno.text, done: false });
+      this.nuovoImpegno.text = "";
+    },
+    rimuoviImpegno(index) {
+      const impegno = this.impegni.splice(index, 1)[0];
+      if (impegno.done) {
+        this.impegniSvolti.push(impegno);
       }
-    }
-  }).mount('#app')
- 
+    },
+    sbarraTesto(index) {
+      if (this.impegni[index].done) {
+        return { 'text-decoration': 'line-through' };
+      } else {
+        return {};
+      }
+    },
+  },
+}).mount("#app");
+
+
+  
